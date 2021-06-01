14714c0d49f9379ba0433aebf35076dc1535c39c
Camilo CV <cama0047@algonquinlive.com>
2021-06-01 12:55:03 -0400

    Testing the useState Hook

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState}from 'react'
 import { Button, Container } from 'reactstrap'
 
 function App() {
+//const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+  const [count, setCount] = useState(0)
+
   return (
     <Container style={{ marginTop: 20 }}>
-      <Button color='success'>Let's start</Button>
+      <p className='text-primary'>You clicked {count} times.</p>
+      <Button onClick={() => setCount(count + 1)} color='success'>
+        Increase the count
+      </Button> 
+      <Button onClick={() => setCount(count - 1)} color='danger'>
+        Decrease the count
+      </Button>
     </Container>
+  
   )
 }
 export default App
