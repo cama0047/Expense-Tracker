36653f35b6a81dc319059624fe8ea7b0e94056d0
Camilo CV <cama0047@algonquinlive.com>
2021-06-01 14:24:52 -0400

    adding handlerName and handlerAmount

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React,{useState}from 'react'
 import { Jumbotron, Container } from 'reactstrap'
+import Form from "./components/Form.js"
+import List from "./components/List.js"
 
 function App() {
 //const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -11,6 +13,25 @@ const ALL_EXPENSES = [
 ]
 
 const [expenses, setExpenses] = useState(ALL_EXPENSES)
+const [name, setName] = useState()
+const [amount, setAmount] = useState()
+
+
+
+const handlerName = (event) =>{
+/*------ PRINT -------*/
+console.log("name: ", event.targt.value);
+setName(event.targt.value);
+
+}
+
+const handlerAmount = (event) =>{
+  /*------ PRINT -------*/
+  console.log("Amount: ", event.targt.value);
+  setAmount(event.targt.value);
+}
+
+
   return (
 
     <Container className='text-center'>
@@ -29,6 +50,8 @@ const [expenses, setExpenses] = useState(ALL_EXPENSES)
             </span>
           </p>
         </div>
+        <Form/>
+        <List expenses={expenses} />
       </Jumbotron>
     </Container>
   
---
+++ components/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+
+/* component that is going to display a list of expense items with their corresponding amount. */
+
+function List({expenses}) {
+    return (
+        <div>
+    <ListGroup>
+      {expenses.map(item => (
+        <ListGroupItem key={item.id}>
+          {item.name} - $ {item.amount}
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  </div>
+    )
+}
+
+export default List
